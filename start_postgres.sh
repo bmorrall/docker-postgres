0da24c8bc44b541256feba83f5be7084531b02e2
Ben Morrall <bemo56@hotmail.com>
2014-06-07 16:03:32 +1000

    Renamed

--- start_postgres.sh
+++ start_postgres.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/lib/postgresql/9.3/bin/postgres -D /var/lib/postgresql/9.3/main -c config_file=/etc/postgresql/9.3/main/postgresql.conf
+/usr/lib/postgresql/9.3/bin/postgres -D /apps/pgdata -c config_file=/etc/postgresql/9.3/main/postgresql.conf
